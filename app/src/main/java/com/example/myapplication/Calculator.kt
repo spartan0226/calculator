f7ec5141c1797ea6b7e7de7153c08313f34871b3
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-03-14 10:59:23 +0900

    Update Calculator.kt

--- app/src/main/java/com/example/myapplication/Calculator.kt
+++ app/src/main/java/com/example/myapplication/Calculator.kt
@@ -1,6 +1,6 @@
 package com.example.myapplication
 
-class Calculator {
+class Cal {
     fun plusOp(num1:Int, num2:Int) {
         val result = num1 + num2
         println("덧셈 결과값은 ${result}입니다.")
