c76508edde75f5183543d2fd497e4ae1183487c7
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-03-14 10:52:18 +0900

    Update Main.kt

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -3,8 +3,28 @@ package com.example.myapplication
 fun main() {
     val calculator = Calculator()
 
-    calculator.plusOp(10, 30)
-    calculator.minusOp(30, 20)
-    calculator.multiOp(40, 10)
-    calculator.divOp(60,20)
+    while(true) {
+        println("[1]덧셈 [2]뺄셈 [3]곱셈 [4]나눗셈")
+        val choice = readLine()!!.toInt()
+
+        println("첫 번째 숫자를 입력하세요.")
+        val num1 = readLine()!!.toInt()
+        println("두 번째 숫자를 입력하세요.")
+        val num2 = readLine()!!.toInt()
+
+        when(choice) {
+            1 -> {
+                calculator.plusOp(num1, num2)
+            }
+            2 -> {
+                calculator.minusOp(num1, num2)
+            }
+            3 -> {
+                calculator.multiOp(num1, num2)
+            }
+            4 -> {
+                calculator.divOp(num1, num2)
+            }
+        }
+    }
 }
