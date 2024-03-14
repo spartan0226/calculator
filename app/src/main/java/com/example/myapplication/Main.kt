5d4c1a9259177d8c2d2b4ba4e2e3d1ff25d9fa09
spartan0226 <161425545+spartan0226@users.noreply.github.com>
2024-03-14 09:31:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+fun main() {
+    val calculator = Calculator()
+
+    calculator.plusOp(10, 30)
+    calculator.minusOp(30, 20)
+    calculator.multiOp(40, 10)
+    calculator.divOp(60,20)
+}
